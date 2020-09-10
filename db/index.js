f66558b4e8207b51eeecddf6dce06247aa817e43
max <xiaoxin88420@hotmail.com>
2020-09-10 11:08:39 -0700

    update mongoDB Atlas

--- db/index.js
+++ db/index.js
@@ -1,4 +1,6 @@
-module.exports = require('mongoose').connect('mongodb://localhost/todo_db', {
+module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/todo_db', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 })
